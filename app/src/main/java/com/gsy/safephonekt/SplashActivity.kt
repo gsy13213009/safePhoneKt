8f49dbc049917ffbd6e8b86020bfe1ed5828666a
郭思义 <gsy@meitu.com>
2017-10-21 13:55:04 +0800

    [update] 新建项目

---
+++ app/src/main/java/com/gsy/safephonekt/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.gsy.safephonekt
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
